8d6728aede390003e2d939f2fb59a068d1d4c079
max <bozzhik@ya.ru>
2023-04-29 16:52:52 +0300

    start make gradient.html && restore getRandom() + onRandomBtn()

--- src/javascript/gradient.js
+++ src/javascript/gradient.js
@@ -1,87 +1,27 @@
-const btn = document.getElementById('btn')
-const fancy = document.getElementById('fancy')
+const randomBtn = document.getElementById('RANDOM_GENERATE_BTN')
+const fancyBtn = document.getElementById('FANCY_GENERATE_BTN')
+const changeble = document.getElementById('CHANGEBLE_GRADIENT')
+const textGradient = document.getElementById('INNER_GRADIENT')
 
-//** changes all h1 text
-// const h1 = document.querySelector('h1')
-//** changes only span in h1 text
-const h1 = document.getElementById('changeble')
-const textGradient = document.querySelector('h2')
-
-function getRandomGradient() {
-  function randomGradient() {
-    function getRandomColor() {
-      let symbols = '0123456789ABCDEF'
-      let color = '#'
-
-      for (let i = 0; i < 6; i++) {
-        color += symbols[Math.floor(Math.random() * 16)]
-      }
-      return color
-    }
-    let color1 = getRandomColor()
-    let color2 = getRandomColor()
-    return `linear-gradient(to right, ${color1}, ${color2})`
+function getRandomColor() {
+  const symbols = '0123456789ABCDEF'
+  let color = '#'
+  for (let i = 0; i < 6; i++) {
+    color += symbols[Math.floor(Math.random() * 16)]
   }
-
-  btn.addEventListener('click', () => {
-    let newGradient = randomGradient()
-    let newClass = document.createElement('style')
-    let className = 'randomGradient' + Date.now()
-    newClass.innerHTML = `.${className} { background: ${newGradient}; }`
-    document.head.appendChild(newClass)
-    h1.className = className
-    h1.classList.add('gradient-text')
-    textGradient.innerHTML = `background: ${newGradient};`
-  })
-}
-getRandomGradient()
-
-function getFancyGradient() {
-  let counter = 0
-  function fancyGradient() {
-    const gradients = [
-      'linear-gradient(90deg, #A020DF 0%, #D721BF 0.01%, #0469E2 100%)',
-      'linear-gradient(90deg, #6A6EFF 0%, #F6AEFE 100%)',
-      'linear-gradient(90deg, #FF950C 0%, #FEF861 100%)',
-      'linear-gradient(90deg, #02EF2D 0%, #01F032 0.01%, #E3E518 100%)',
-      'linear-gradient(90deg, #505ADD 0%, #804FD0 30.64%, #BC40BF 65%, #F4A033 100%)',
-      'linear-gradient(90deg, #EF1478 0%, #E61479 0.01%, #8321EA 100%)',
-      'linear-gradient(90deg, #ED7373 0%, #EE0E86 100%)',
-      'linear-gradient(90deg, #15FAFE 0%, #31BAFE 30.64%, #665AFD 65%, #9D09FF 100%)',
-      'linear-gradient(90deg, #D54AFF 0%, #7103EF 100%)',
-      'linear-gradient(90deg, #15B5E9 0%, #10F75D 100%)',
-      'linear-gradient(90deg, #F7C837 0%, #F14A75 100%)'
-    ]
-
-    let currentIndex = counter
-    counter = (counter + 1) % gradients.length
-    return gradients[currentIndex]
-  }
-
-  fancy.addEventListener('click', () => {
-    let newGradient = fancyGradient()
-    let newClass = document.createElement('style')
-    let className = 'randomGradient' + Date.now()
-    newClass.innerHTML = `.${className} { background: ${newGradient}; }`
-    document.head.appendChild(newClass)
-    h1.className = className
-    h1.classList.add('gradient-text')
-    textGradient.innerHTML = `background: ${newGradient};`
-  })
-}
-getFancyGradient()
-
-function copyGradient() {
-  textGradient.addEventListener('click', () => {
-    navigator.clipboard.writeText(textGradient.innerHTML).then(
-      () => {
-        alert('Gradient copied!')
-      },
-      () => {
-        alert('Error! Copy it yourself :(')
-      }
-    )
-  })
+  return color
 }
 
-copyGradient()
+function onRandomBtn() {
+  const color1 = getRandomColor()
+  const color2 = getRandomColor()
+  const newGradient = `linear-gradient(to right, ${color1}, ${color2})`
+  const newClass = `randomGradient${Date.now()}`
+  const style = `.${newClass} { background: ${newGradient}; }`
+  document.head.insertAdjacentHTML('beforeend', `<style>${style}</style>`)
+  changeble.className = newClass
+  changeble.classList.add('GRADIENT_TEXT')
+  textGradient.innerHTML = `background: ${newGradient};`
+}
+
+randomBtn.addEventListener('click', onRandomBtn)
